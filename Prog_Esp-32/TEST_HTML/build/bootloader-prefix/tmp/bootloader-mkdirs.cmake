ef8e8fbc5b0e321fa7d9a04a1de8f9ca0f2af5ac
Jabooby <turcottejacob99@gmail.com>
2023-03-14 08:20:47 -0400

    Kicad, PA et site web

---
+++ Prog_Esp-32/TEST_HTML/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/turco/esp/esp-idf/components/bootloader/subproject"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/tmp"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/src"
+  "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/Session_6/Projet/EcouSol/Prog_Esp-32/TEST_HTML/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
